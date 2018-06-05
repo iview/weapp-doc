3e22d806f3caa65f179dbe471bceef7f66cf55fb
梁灏 <admin@aresn.com>
2018-06-05 17:30:24 +0800

    release 1.1.0

--- src/views/guide/update.vue
+++ src/views/guide/update.vue
@@ -24,6 +24,21 @@
         <h1>更新日志</h1>
         <article class="doc-update">
             <Timeline pending>
+                <Timeline-item>
+                    <Anchor title="1.1.0" h2></Anchor>
+                    <p>
+                        <code>2018-06-05</code>
+                    </p>
+                    <ul>
+                        <li>Cell 的点击事件名改为了 <code>bind:click</code>。</li>
+                        <li>Button 的属性 <code>showMessageCard</code> 类型修正为 Boolean。</li>
+                        <li>Drawer 的属性 <code>show</code> 改名为 <code>visible</code>, <code>type</code> 改名为 <code>mode</code>, <code>overlay</code> 改名为 <code>mask</code>, <code>closeOverlay</code> 改名为 <code>maskClosable</code>。</li>
+                        <li>Rate 支持触摸滑动来选择了。</li>
+                        <li>Alert 的关闭事件名改为了 <code>bind:close</code>。</li>
+                        <li>修复 Alert 缺失 info 图标的 bug。</li>
+                        <li>修复 Page 警告的问题。</li>
+                    </ul>
+                </Timeline-item>
                 <Timeline-item color="green">
                     <Icon type="trophy" size="18" slot="dot"></Icon>
                     <Anchor title="1.0.0" h2></Anchor>
